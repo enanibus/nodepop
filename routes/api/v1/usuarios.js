134258584b65d7f6bfea910bcb8b9bc00bf2ebb9
jacobo enriquez gabeiras <michi_two@yahoo.com>
2016-05-03 00:16:37 +0200

    Correción autenticación y manejo de errores

--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -20,18 +20,14 @@ router.post('/authenticate', function(req, res) {
 
     Usuario.findOne({nombre: nombre}, function(err, user) {
         if (err) {
-            return res.status(500).json({success: false, error: err});
+            return errorHandler(new Error('Internal server error'), req, res, 500);
         }
-
         if (!user) {
             return errorHandler(new Error('Authentication failed. User not found'), req, res, 401);
         }
-
         if (sha256(clave) !== user.clave) {
-            //return res.status(401).json({success: false, error: 'Authentication failed, Invalid password'});
             return errorHandler(new Error('Authentication failed, Invalid password'), req, res, 401);
         }
-
         let token = jwt.sign({id: user._id}, config.jwt.secret, { expiresIn: '2 days' });
         res.json({ success: true, token: token });
     });
