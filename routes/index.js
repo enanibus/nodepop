04d45c29379e44dc0e3738312227d976105dfe65
jacobo enriquez gabeiras <michi_two@yahoo.com>
2016-05-08 19:23:02 +0200

    Presentación de wiki e index.html

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'nodepop API v1' });
+  res.render('index', { title: 'nodepop API' });
 });
 
 module.exports = router;
